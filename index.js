b9e3118f9a08b6cc348f6a3494ce5d4cf935488a
Maraisa Ferreira <ferreira.maraisacristina@gmail.com>
2023-03-28 10:37:52 -0300

    set flash messages & session

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const session = require('express-session');
 const flash = require('express-flash');
-const fileStore = require('session-file-store')(session);
+const FileStore = require('session-file-store')(session);
 
 const conn = require('./db/conn');
 
@@ -13,6 +13,38 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//set flash messages
+app.use(flash());
+
+//set session
+app.use(
+	session({
+		name: 'session',
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: false,
+		store: new FileStore({
+			logFn: function () {},
+			path: require('path').join(require('os').tmpdir(), 'sessions'),
+		}),
+		cookie: {
+			secure: false,
+			httpOnly: true,
+			maxAge: 360000,
+			expires: new Date(Date.now() + 360000),
+		},
+	}),
+);
+
+//set session to res
+app.use((req, res, next) => {
+	if (req.session.userid) {
+		res.locals.session = req.session;
+	}
+
+	next();
+});
 
 conn.sync().then(() => {
 	console.log('Conectado ao DB...');
