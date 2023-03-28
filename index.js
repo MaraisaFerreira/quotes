34dce2cdaad6ca5a0682c8172457916e7358346e
Maraisa Ferreira <ferreira.maraisacristina@gmail.com>
2023-03-28 10:06:42 -0300

    set engine & body json

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 require('dotenv').config();
+const session = require('express-session');
+const flash = require('express-flash');
+const fileStore = require('session-file-store')(session);
 
 const conn = require('./db/conn');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 conn.sync().then(() => {
 	console.log('Conectado ao DB...');
